59239a0f77f5b6dea7aa9c80c964440a8ee45ffe
Patrick Altman <paltman@gmail.com>
2014-10-31 01:23:41 -0500

    Update for Django 1.7

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -14,18 +14,7 @@ framework.
 
 """
 import os
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
-import {{ project_name }}.startup as startup
-startup.run()
-
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
